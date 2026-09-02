1e03bf30e70575c4c5718d8806cb72a8ee56e1ed
Alejandro Mota <alejandrocastillomota@gmail.com>
2026-09-02 07:12:31 -0600

    fix(parcial01): Correccion todo y actualizar al 1 de sep los archivos

--- src/EventsHub.Persistence/Migrations/20260902035943_InitialCreate.Designer.cs
+++ src/EventsHub.Persistence/Migrations/20260902035943_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EventsHub.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260828131904_InitialCreate")]
+    [Migration("20260902035943_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
